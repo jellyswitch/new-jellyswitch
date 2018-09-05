51691233a05052e4b530916906dc3b98d99be420
Dave Paola <dpaola2@gmail.com>
2018-09-05 13:21:17 -0700

    theming

--- config/initializers/customization.rb
+++ config/initializers/customization.rb
@@ -5,3 +5,13 @@ Rails.application.config.x.customization.wifi_name = ENV['WIFI_NAME'] || "not se
 Rails.application.config.x.customization.wifi_password = ENV['WIFI_PASSWORD'] || "not set"
 Rails.application.config.x.customization.building_address = ENV['BUILDING_ADDRESS'] || "not set"
 Rails.application.config.x.customization.logo = ENV['INSTANCE_LOGO']
+
+# Theming
+Rails.application.config.x.customization.primary = ENV['THEME_PRIMARY']
+Rails.application.config.x.customization.secondary = ENV['THEME_SECONDARY']
+Rails.application.config.x.customization.success = ENV['THEME_SUCCESS']
+Rails.application.config.x.customization.danger = ENV['THEME_DANGER']
+Rails.application.config.x.customization.info = ENV['THEME_INFO']
+Rails.application.config.x.customization.warning = ENV['THEME_WARNING']
+Rails.application.config.x.customization.light = ENV['THEME_LIGHT']
+Rails.application.config.x.customization.dark = ENV['THEME_DARK']
