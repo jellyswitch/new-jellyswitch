69018e1d4e5deceaf116261b5a6715728056f0f4
Dave Paola <dpaola2@gmail.com>
2019-02-04 10:34:47 -0800

    add google maps image to frontpage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def pretty_datetime(input)
     input.strftime("%m/%d/%Y at %l:%M%P")
   end
+
+  def google_map(center)
+    key = ENV['GOOGLE_MAPS_API_KEY']
+    "https://maps.googleapis.com/maps/api/staticmap?key=#{key}&markers=size:small%7Ccolor:red%7C#{center}&size=500x500&zoom=17"    
+  end
 end
