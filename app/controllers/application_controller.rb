3fcda3dc6eb9d796c598a744291af841670d934b
Dave Paola <dpaola2@gmail.com>
2019-02-05 16:58:37 -0800

    friendlier unauthorized message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def user_not_authorized
-    flash[:alert] = "Sorry! You are not authorized to do that."
+    flash[:alert] = "Whoops! That's not allowed. If this isn't what you were expecting, please contact #{current_tenant.contact_name} by calling #{current_tenant.contact_phone}."
     redirect_to referrer_or_root
   end
 
