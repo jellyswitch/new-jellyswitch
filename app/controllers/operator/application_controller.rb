73847973ed2c3eeba35b85d9e9f022aa07ce71b6
Dave Paola <dpaola2@gmail.com>
2019-02-07 17:16:27 -0800

    move background_image helper up into application controller

--- app/controllers/operator/application_controller.rb
+++ app/controllers/operator/application_controller.rb
@@ -1,8 +1,4 @@
 class Operator::ApplicationController < ::ApplicationController
   set_current_tenant_by_subdomain(:operator, :subdomain)
   layout "operator"
-
-  def background_image
-    @background_image = current_tenant.background
-  end
 end
