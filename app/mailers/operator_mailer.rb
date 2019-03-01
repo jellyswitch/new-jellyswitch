d26b64093beeb7fcda12562c842b098851e79762
Dave Paola <dpaola2@gmail.com>
2019-02-28 17:24:00 -0800

    update from address for new demo email notirication

--- app/mailers/operator_mailer.rb
+++ app/mailers/operator_mailer.rb
@@ -1,4 +1,5 @@
 class OperatorMailer < ApplicationMailer
+  default from: 'Dave at Jellyswitch <dave@jellyswitch.com>'
   def new_demo_instance(user, operator)
     @user = user
     @operator = operator
