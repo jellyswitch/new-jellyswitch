1ee1860b0678d59289dfc209027d21c79d21050f
Dave Paola <dpaola2@gmail.com>
2019-03-21 08:57:24 -0700

    prorate memberships

--- app/interactors/switch_membership.rb
+++ app/interactors/switch_membership.rb
@@ -5,21 +5,34 @@ class SwitchMembership
     old_subscription = context.old_subscription
     new_subscription = context.new_subscription
 
-    new_subscription.user = old_subscription.user # in case this is an admin
+    ActiveRecord::Base.transaction do
+      old_subscription.active = false
+      new_subscription.active = true
 
-    result = CreateSubscription.call(
-      subscription: new_subscription,
-      user: old_subscription.user,
-      token: nil
-    )
+      new_subscription.user_id = old_subscription.user_id # hack for admins switching memberships on behalf of users
 
-    if result.success?
-      result = CancelSubscription.call(subscription: old_subscription)
-      if !result.success?
-        context.fail!(message: result.message)
-      end
-    else
-      context.fail!(message: result.message)
+      new_subscription.stripe_subscription_id = old_subscription.stripe_subscription_id
+
+      new_subscription.save!
+      old_subscription.save!
+
+      Stripe::Subscription.update(
+        new_subscription.stripe_subscription_id,
+        {
+          cancel_at_period_end: false,
+          items: [
+            {
+              id: new_subscription.stripe_subscription.items.data[0].id,
+              plan: new_subscription.plan.stripe_plan_id
+            }
+          ],
+      },{
+        api_key: new_subscription.plan.operator.stripe_secret_key,
+        stripe_account: new_subscription.plan.operator.stripe_user_id
+      })
+
+    rescue Exception => e
+      context.fail!(message: "Unable to switch membership: #{e.message}")
     end
   end
 end
