1d0fe131e31b6a6de9fc5238d5621d09f27b7c74
Dave Paola <dpaola2@gmail.com>
2019-03-25 17:25:55 -0700

    make it work with turbolinks

--- app/controllers/operator/search_results_controller.rb
+++ app/controllers/operator/search_results_controller.rb
@@ -7,7 +7,12 @@ class Operator::SearchResultsController < Operator::BaseController
 
   def create
     @query = params[:query]
+    turbolinks_redirect(query_search_results_path(query: @query), action: "replace")
+  end
+
+  def query
+    @query = params[:query]
     @results = FeedItem.search(@query, fields: [:text, :user_name, :type, :amount])
-    
+    render :create, status: 200
   end
 end
