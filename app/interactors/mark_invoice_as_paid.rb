8a073d8c2079a79c3606a2830531c577ed1386f6
Dave Paola <dpaola2@gmail.com>
2019-04-08 12:37:33 -0700

    fix bug preventing invoices from being marked as paid, closes #225

--- app/interactors/mark_invoice_as_paid.rb
+++ app/interactors/mark_invoice_as_paid.rb
@@ -2,13 +2,14 @@ class MarkInvoiceAsPaid
   include Interactor
 
   def call
-    invoice = Invoice.find(context.invoice_id).stripe_invoice
+    invoice = Invoice.find(context.invoice_id)
+    stripe_invoice = invoice.stripe_invoice
 
-    if invoice.customer != context.user.stripe_customer_id
+    if stripe_invoice.customer != context.user.stripe_customer_id
       context.fail!(message: "Invalid invoice.")
     end
 
-    invoice.pay({paid_out_of_band: true})
+    stripe_invoice.pay({paid_out_of_band: true})
     invoice.update(status: "paid")
     context.invoice = invoice
   end
