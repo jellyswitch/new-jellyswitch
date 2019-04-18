86d84398b615450c19a957971cbac1488106991f
raderj89 <raderj89@gmail.com>
2019-04-18 14:47:21 -0700

    update data migration

--- db/migrate/20190416170418_add_location_and_resources_for_spaces.rb
+++ db/migrate/20190416170418_add_location_and_resources_for_spaces.rb
@@ -1,43 +1,35 @@
 class AddLocationAndResourcesForSpaces < ActiveRecord::Migration[5.2]
+  LOCATION_RESOURCES = %w(rooms offices office_leases member_feedbacks feed_items doors)
+
   def up
-    operator = Operator.find_by subdomain: 'demo2'
-
-    location = operator.locations.create!(
-      operator.attributes.except(*%w(
-        id
-        android_url
-        approval_required
-        day_pass_cost_in_cents
-        email_enabled
-        ios_url
-        kisi_api_key
-        subdomain
-        created_at
-        updated_at
-        stripe_user_id
-      )
-    ).merge(city: 'South Lake Tahoe', state: 'CA', zip: '96150'))
-
-    operator.locations.create!(
-      building_address: '110 Sutter St',
-      city: 'San Francisco',
-      state: 'CA',
-      zip: '94104',
-      wifi_name: 'wifi',
-      wifi_password: 'wifi',
-      square_footage: 2000,
-      name: 'BlocWork',
-      contact_email: 'loree.smith@example.net'
-    )
-
-    %w(rooms offices office_leases member_feedbacks feed_items doors).each do |resource|
+    LOCATION_RESOURCES.each do |resource|
       add_reference :"#{resource}", :location, foreign_key: true
+      resource.singularize.camelize.constantize.reset_column_information
+    end
 
-      resource.camelize.singularize.constantize.reset_column_information
+    Operator.all.each do |operator|
+      puts "Creating location and location resources for #{operator.name} (ID: #{operator.id})"
+      location = operator.locations.create!(
+        operator.attributes.except(*%w(
+          id
+          android_url
+          approval_required
+          day_pass_cost_in_cents
+          email_enabled
+          ios_url
+          kisi_api_key
+          subdomain
+          created_at
+          updated_at
+          stripe_user_id
+        )
+      ))
 
-      operator.public_send(resource).each do |model|
-        model.location_id = location.id
-        model.save!
+      LOCATION_RESOURCES.each do |resource|
+        operator.public_send(resource).each do |model|
+          model.location_id = location.id
+          model.save!
+        end
       end
     end
   end
