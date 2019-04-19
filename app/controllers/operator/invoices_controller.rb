a918734dc840a922061aaddc4f98c1ca9c12bf0e
Dave Paola <dpaola2@gmail.com>
2019-04-19 08:26:48 -0700

    paginate invoices, closes #266

--- app/controllers/operator/invoices_controller.rb
+++ app/controllers/operator/invoices_controller.rb
@@ -1,24 +1,24 @@
 class Operator::InvoicesController < Operator::BaseController
   def index
-    @invoices = Invoice.all.order('date DESC')
+    @pagy, @invoices = pagy(Invoice.all.order('date DESC'))
     authorize @invoices
     background_image
   end
 
   def due
-    @invoices = Invoice.due.order('date DESC')
+    @pagy, @invoices = pagy(Invoice.due.order('date DESC'))
     authorize @invoices
     background_image
   end
 
   def recent
-    @invoices = Invoice.recent.order('date DESC')
+    @pagy, @invoices = pagy(Invoice.recent.order('date DESC'))
     authorize @invoices
     background_image
   end
 
   def delinquent
-    @invoices = Invoice.delinquent.order('date DESC')
+    @pagy, @invoices = pagy(Invoice.delinquent.order('date DESC'))
     authorize @invoices
     background_image
   end
