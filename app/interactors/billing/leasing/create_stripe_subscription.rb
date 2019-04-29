f3622ef3ab02fa5933abcf4c2841abe032adfa90
Dave Paola <dpaola2@gmail.com>
2019-04-29 11:51:04 -0700

    update leases to use next invoice date

--- app/interactors/billing/leasing/create_stripe_subscription.rb
+++ app/interactors/billing/leasing/create_stripe_subscription.rb
@@ -6,13 +6,9 @@ class Billing::Leasing::CreateStripeSubscription
   def call
     subscription = office_lease.subscription
     organization = subscription.subscribable
-    start_date = office_lease.start_date
+    initial_invoice_date = office_lease.initial_invoice_date
 
-    if start_date < Time.current
-      stripe_start_date = Time.zone.at(1.month.from_now.beginning_of_month + 2.hours).to_i
-    else
-      stripe_start_date = (Time.zone.at(start_date.to_time.to_i) + 2.hours).to_i
-    end
+    stripe_start_date = (Time.zone.at(initial_invoice_date.to_time.to_i) + 2.hours).to_i
 
     stripe_subscription = operator.create_stripe_subscription(organization, subscription, stripe_start_date)
     subscription.update(stripe_subscription_id: stripe_subscription.id)
