a2d4539baddade8b922701c232b1024cefafb8c7
Dave Paola <dpaola2@gmail.com>
2019-05-21 13:48:04 -0700

    ability to specify an optional day limit for plans, refs T15

---
+++ db/migrate/20190521200229_add_day_limit_to_plans.rb
@@ -0,0 +1,6 @@
+class AddDayLimitToPlans < ActiveRecord::Migration[5.2]
+  def change
+    add_column :plans, :has_day_limit, :boolean, default: false, null: false
+    add_column :plans, :day_limit, :integer, null: false, default: 0
+  end
+end
