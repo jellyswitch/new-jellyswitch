6160204d4a547f76c4bc4dac9aad460e67aa14d7
Dave Paola <dpaola2@gmail.com>
2019-05-27 13:30:07 -0700

    checkins are paid for by billable, refs T44

---
+++ app/adapters/check_inable/default_checkin.rb
@@ -0,0 +1,16 @@
+module DayPassable
+  class CheckInable::DefaultCheckin < SimpleDelegator
+    attr_accessor :checkin
+
+    def initialize(checkin)
+      @checkin = checkin
+    end
+
+    def invoice_args
+      {
+        customer: checkin.billable.stripe_customer_id,
+        auto_advance: true
+      }
+    end
+  end
+end
---
+++ app/adapters/check_inable/out_of_band.rb
@@ -0,0 +1,8 @@
+class CheckInable::OutOfBand < CheckInable::DefaultCheckin
+def invoice_args
+    super.merge!(
+      billing: 'send_invoice',
+      days_until_due: 30
+    )
+  end
+end
