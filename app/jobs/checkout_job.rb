1267471f5398602ceadca64b50a019c51eed6148
Dave Paola <dpaola2@gmail.com>
2019-05-28 16:47:18 -0700

    auto checkout job

---
+++ app/jobs/checkout_job.rb
@@ -0,0 +1,22 @@
+class CheckoutJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    Operator.production.all.each do |operator|
+      operator.locations.each do |location|
+        location.checkins.open.each do |checkin|
+          Time.use_zone(location.time_zone) do
+            timestamp =  Time.parse(location.working_day_end)
+
+            result = Checkins::Checkout.call(checkin: checkin, datetime_out: timestamp)
+
+            if !result.success?
+              puts result.message
+              Rollbar.error("Error auto-checking out: #{result.message}")
+            end
+          end
+        end
+      end
+    end
+  end
+end
