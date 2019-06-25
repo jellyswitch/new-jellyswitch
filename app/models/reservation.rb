5781b768d17af09de8f1f1cdf370649f6b42a995
Dave Paola <dpaola2@gmail.com>
2019-06-25 15:57:10 -0700

    use new hours standin

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,6 @@
 #  cancelled   :boolean          default(FALSE), not null
 #  datetime_in :datetime         not null
 #  hours       :integer          default(1), not null
-#  minutes     :integer          default(0), not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  room_id     :integer          not null
@@ -49,5 +48,7 @@ class Reservation < ApplicationRecord
     Room.unscoped { super }
   end
 
-  
+  def hours
+    minutes.to_f / 60.0
+  end
 end
