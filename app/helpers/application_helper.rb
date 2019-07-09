ceb11aa7c506f7b944c0ae5571cd5f13ed6a021e
Dave Paola <dpaola2@gmail.com>
2019-07-09 12:53:26 -0700

    bunch of shit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@
 module ApplicationHelper
   include PlansHelper
   include LandingHelper
+  include LayoutHelper
   include Pagy::Frontend
 
   def pretty_datetime(input)
@@ -44,57 +45,6 @@ module ApplicationHelper
     else
       nil
     end
-  end
-
-  def card
-    render "layouts/card" do
-      yield
-    end
-  end
-
-  def super_wide_card
-    render "layouts/super_wide_card" do
-      yield
-    end
-  end
-
-  def card_wrapper
-    render "layouts/card_wrapper" do
-      yield
-    end
-  end
-
-  def wide_card
-    render "layouts/wide_card" do
-      yield
-    end
-  end
-
-  def list_wrapper
-    render "layouts/list_wrapper" do
-      yield
-    end
-  end
-
-  def list_item
-    render "layouts/list_item" do
-      yield
-    end
-  end
-
-  def feed_item_card(feed_item, comments)
-    render "operator/feed_items/feed_item_card", feed_item: feed_item, comments: comments
-  end
-
-  def breadcrumb
-    render "layouts/breadcrumb" do
-      yield
-    end
-  end
-
-  def title(page_title)
-    content_for(:title) { page_title }
-    page_title
   end
 
   def stripe_oauth_url(operator)
