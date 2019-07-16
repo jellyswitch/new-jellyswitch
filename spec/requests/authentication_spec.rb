5d17cd7c02bfdd60e1660a053a8d18b39805f048
Dave Paola <dpaola2@gmail.com>
2019-07-15 19:56:04 -0700

    fix auth spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -4,24 +4,27 @@ require 'rails_helper'
 RSpec.describe 'Authentication', type: :request do
   context "for an admin" do
     let(:subdomain) { create(:subdomain) }
-    let(:admin) { create(:user, :admin) }
     let(:operator) { create(:operator, :with_location, :with_individual_plans, subdomain: subdomain.subdomain) }
+    let(:admin) { create(:user, :admin) }
 
     before do
       ActsAsTenant.default_tenant = operator
+      admin.update(operator_id: operator.id)
     end
 
     after do
       ActsAsTenant.default_tenant = nil
     end
 
-    it "redirects to the management feed with a correct password" do
+    it "redirects to landing path with a correct password" do
       post login_path, params: { session: { email: admin.email, password: 'password' } }
-      puts response.inspect
-      expect(response).to redirect_to(feed_items_path)
+      expect(response).to redirect_to(landing_path)
     end
 
-    it "re-renders the login page with an incorrect password"
+    it "re-renders the login page with an incorrect password" do
+      post login_path, params: { session: { email: admin.email, password: 'wrongpassword' } }
+      expect(response).to render_template(:new)
+    end
   end
 
   context "for a superadmin"
