4cce2dd34a4f4e523d85740688f5ec928da4c8b9
Dave Paola <dpaola2@gmail.com>
2019-11-04 14:54:22 -0800

    refactor

--- app/adapters/stripe_subscription/default_subscription.rb
+++ app/adapters/stripe_subscription/default_subscription.rb
@@ -8,17 +8,20 @@ module StripeSubscription
     end
 
     def subscription_args
-      if subscription.start_date == Time.zone.today
-        billing_cycle_anchor = nil
-      else
-        billing_cycle_anchor = subscription.start_date.to_time.to_i
-      end
       {
         customer: subscription.billable.stripe_customer_id,
         items: [{ plan: subscription.plan.stripe_plan_id }],
         prorate: false,
         billing_cycle_anchor: billing_cycle_anchor
       }
+    end
+
+    def billing_cycle_anchor
+      if subscription.start_date == Time.zone.today
+        nil
+      else
+        subscription.start_date.to_time.to_i
+      end
     end
   end
 end
