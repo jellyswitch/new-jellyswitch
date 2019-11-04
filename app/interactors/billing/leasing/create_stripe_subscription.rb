413204ed8a3f82bee96ea69152dd444c401878be
Dave Paola <dpaola2@gmail.com>
2019-11-04 15:01:37 -0800

    fix subscriptions created from leases

--- app/interactors/billing/leasing/create_stripe_subscription.rb
+++ app/interactors/billing/leasing/create_stripe_subscription.rb
@@ -11,7 +11,7 @@ class Billing::Leasing::CreateStripeSubscription
 
     stripe_start_date = (Time.zone.at(initial_invoice_date.to_time.to_i) + 2.hours).to_i
 
-    stripe_subscription = operator.create_stripe_subscription(subscription, stripe_start_date)
+    stripe_subscription = operator.create_stripe_subscription(subscription)
     subscription.update(stripe_subscription_id: stripe_subscription.id)
   rescue StandardError => e
     context.fail!(message: e.message)
