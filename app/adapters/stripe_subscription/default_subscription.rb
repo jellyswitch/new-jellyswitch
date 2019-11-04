76f535c5c4fec86f7e611a0d64fcec7e3598357a
Dave Paola <dpaola2@gmail.com>
2019-11-04 14:52:53 -0800

    restyle subscription form and fix bug where membership failed if you want to start today

--- app/adapters/stripe_subscription/default_subscription.rb
+++ app/adapters/stripe_subscription/default_subscription.rb
@@ -8,11 +8,16 @@ module StripeSubscription
     end
 
     def subscription_args
+      if subscription.start_date == Time.zone.today
+        billing_cycle_anchor = nil
+      else
+        billing_cycle_anchor = subscription.start_date.to_time.to_i
+      end
       {
         customer: subscription.billable.stripe_customer_id,
         items: [{ plan: subscription.plan.stripe_plan_id }],
         prorate: false,
-        billing_cycle_anchor: subscription.start_date.to_time.to_i
+        billing_cycle_anchor: billing_cycle_anchor
       }
     end
   end
