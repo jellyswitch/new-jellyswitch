4cb09dc1552aa2323406573b89f77355f64157a7
Dave Paola <dpaola2@gmail.com>
2019-12-12 14:33:38 -0800

    refactor building access and checkin instructions to be separate

--- app/components/access_instructions/door_buttons.rb
+++ app/components/access_instructions/door_buttons.rb
@@ -1,15 +1,15 @@
 class AccessInstructions::DoorButtons < ApplicationComponent
   include ApplicationHelper
-  
-  def initialize(operator:, location:, mobile_app_request:, show_doors:, doors:)
+
+  def initialize(operator:, location:, mobile_app_request:, has_access:, doors:)
     @operator = operator
     @location = location
     @mobile_app_request = mobile_app_request
-    @show_doors = show_doors
+    @has_access = has_access
     @doors = doors
   end
 
   private
 
-  attr_reader :operator, :location, :mobile_app_request, :show_doors, :doors
+  attr_reader :operator, :location, :mobile_app_request, :has_access, :doors
 end
