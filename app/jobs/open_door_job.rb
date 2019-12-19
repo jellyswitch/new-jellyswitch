258c534e9478e164bad38378bda77f5d562d0a73
Dave Paola <dpaola2@gmail.com>
2019-12-19 12:13:41 -0800

    add logging to door punches

--- app/jobs/open_door_job.rb
+++ app/jobs/open_door_job.rb
@@ -3,14 +3,16 @@ class OpenDoorJob < ApplicationJob
   queue_as :default
   throttle threshold: 1, period: 5.seconds, drop: true
 
-  def perform(door)
+  def perform(door, user)
     url = "https://api.getkisi.com/locks/#{door.kisi_id}/unlock"
     headers = {
       'Accept' => 'application/json',
       'Content-type' => 'application/json',
       'Authorization' => "KISI-LOGIN #{door.operator.kisi_api_key}"
     }
-    HTTParty.post(url, headers: headers)
+    response = HTTParty.post(url, headers: headers)
+    json = JSON.parse(response, symbolize_names: true)
+    DoorPunch.create!(user: current_user, door: @door, json: json)
   rescue StandardError => e
     Rollbar.error(e.message)
   end
