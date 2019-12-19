131e4fd8cdfbbcd9c1c20b3a99fe93471a61822a
Dave Paola <dpaola2@gmail.com>
2019-12-19 12:20:51 -0800

    fix door punch code

--- app/jobs/open_door_job.rb
+++ app/jobs/open_door_job.rb
@@ -2,17 +2,12 @@
 class OpenDoorJob < ApplicationJob
   queue_as :default
   throttle threshold: 1, period: 5.seconds, drop: true
+  include DoorsHelper
 
   def perform(door, user)
-    url = "https://api.getkisi.com/locks/#{door.kisi_id}/unlock"
-    headers = {
-      'Accept' => 'application/json',
-      'Content-type' => 'application/json',
-      'Authorization' => "KISI-LOGIN #{door.operator.kisi_api_key}"
-    }
-    response = HTTParty.post(url, headers: headers)
+    response = HTTParty.post(url(door), headers: headers(door))
     json = JSON.parse(response, symbolize_names: true)
-    DoorPunch.create!(user: current_user, door: @door, json: json)
+    DoorPunch.create!(user: user, door: @door, json: json)
   rescue StandardError => e
     Rollbar.error(e.message)
   end
