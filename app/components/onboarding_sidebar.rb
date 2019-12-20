57dfcd0684817b505b99fdedc84e64356b053056
Dave Paola <dpaola2@gmail.com>
2019-12-20 12:22:39 -0800

    check for modules enabled for onboarding sidebar

--- app/components/onboarding_sidebar.rb
+++ app/components/onboarding_sidebar.rb
@@ -13,4 +13,20 @@ class OnboardingSidebar < ApplicationComponent
   def billing_enabled?
     operator.production? && operator.subdomain != "southlakecoworking"
   end
+
+  def show_day_pass_types?
+    billing_enabled? && operator.day_pass_types.count < 1
+  end
+
+  def show_rooms?
+    billing_enabled? && operator.rooms_enabled? && location.rooms.count < 1
+  end
+
+  def show_doors?
+    billing_enabled? && operator.door_integration_enabled? && location.doors.count < 1
+  end
+
+  def show_members?
+    billing_enabled? && operator.users.members.count < 1
+  end
 end
