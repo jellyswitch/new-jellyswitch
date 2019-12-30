41b5809a0ec2fbc92aff82c5e764b9469f94a33b
Dave Paola <dpaola2@gmail.com>
2019-12-30 11:02:15 -0800

    fix annual billing

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -8,7 +8,14 @@ module SubscriptionsHelper
     if params[:subscription][:start_day].present?
       Time.zone.at(params[:subscription][:start_day].to_i) + 2.hours
     else
-      Time.zone.now + 2.hours
+      if params[:subscription]["start_day(1i)"].present?
+        year = params[:subscription]["start_day(1i)"].to_i
+        month = params[:subscription]["start_day(2i)"].to_i
+        day = params[:subscription]["start_day(3i)"].to_i
+        Time.zone.at(Date.new(year, month, day).beginning_of_day)
+      else
+        Time.zone.now + 2.hours
+      end
     end
   end
 end
