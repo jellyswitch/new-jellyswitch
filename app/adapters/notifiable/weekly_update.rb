8b417fa6e6735aa7465c60b1e2c86fdd2dd7e4e2
Dave Paola <dpaola2@gmail.com>
2020-01-06 10:12:16 -0800

    fix broken weekly update notification code

--- app/adapters/notifiable/weekly_update.rb
+++ app/adapters/notifiable/weekly_update.rb
@@ -5,7 +5,7 @@ module Notifiable
 
     def create_feed_item
       blob = {type: "weekly-update", weekly_update_id: id}
-      user = User.first # Dave???
+      user = ::User.first # Dave???
       FeedItemCreator.create_feed_item(operator, user, blob, created_at: created_at)
     end
 
