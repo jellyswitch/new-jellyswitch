258f97298339cbf2565e3b31365f2db5ba8b6e54
Dave Paola <dpaola2@gmail.com>
2020-01-15 09:00:18 -0800

    fix broken membership_text helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -155,7 +155,7 @@ module ApplicationHelper
 
   def membership_text(plan)
     if plan.operator.membership_text.present?
-      "Memberships start at #{display_price(plan)} and include #{current_tenant.membership_text}."
+      "Memberships start at #{display_price(plan)} and include #{plan.operator.membership_text}."
     else
       "Memberships start at #{display_price(plan)} and vary from flexible desk space to full private offices."
     end
