18e77cd7d0baa7da29abdb1021105ce29993eb09
Dave Paola <dpaola2@gmail.com>
2020-02-21 11:29:14 -0800

    show screen

--- app/controllers/operator/leads_controller.rb
+++ app/controllers/operator/leads_controller.rb
@@ -5,9 +5,17 @@ class Operator::LeadsController < Operator::BaseController
     find_leads
   end
 
+  def show
+    find_lead
+  end
+
   private
 
   def find_leads
     @leads = current_tenant.leads.order("created_at DESC").all
+  end
+
+  def find_lead(key=:id)
+    @lead = current_tenant.leads.find(params[key])
   end
 end
