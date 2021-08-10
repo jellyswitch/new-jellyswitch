1d666810759913b17072978dd2d82068610b5ce0
Ben Damman <ben.damman@gmail.com>
2021-08-09 19:43:46 -0600

    Added apropriate license comments to key files.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,17 @@
-# The test environment is used exclusively to run your application's
-# test suite. You never need to work with it otherwise. Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs. Don't rely on the data there!
+# Copyright (C) 2020  VO Services LLC  www.jellyswitch.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
