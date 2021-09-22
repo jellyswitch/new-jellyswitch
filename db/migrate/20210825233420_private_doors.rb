10eb1922b6f98d02b5fccd26df675b4ae96b3a5c
Ben Damman <ben.damman@gmail.com>
2021-09-22 11:56:02 -0600

    migration fix and ruby 2.7.4 upgrade

--- db/migrate/20210825233420_private_doors.rb
+++ db/migrate/20210825233420_private_doors.rb
@@ -1,6 +1,6 @@
 class PrivateDoors < ActiveRecord::Migration[6.1]
   def change
     add_column :doors, :private, :boolean
-    belongs_to :doors, :private_owner, polymorphic: true, null: true
+    add_reference :doors, :private_owner, polymorphic: true, null: true
   end
 end
