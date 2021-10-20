c1304db4d5ef72af44e1a17511a9ce001c773a08
Ben Damman <ben@aliensfromthefuture.com>
2021-10-20 18:21:46 -0500

    Bumped Rails version from 6.0.2.1 to 6.0.4.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 require "action_view/component/base"
 
-module Bristlecone
+module Jellyswitch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
