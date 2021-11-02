bfbce6d34c6f50468c0af177eb65ec80bc6cf796
Ben Damman <ben.damman@gmail.com>
2021-11-01 20:31:30 -0600

    WIP: added logging into stripe oauth code paths

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -15,10 +15,19 @@ class LandingController < ApplicationController
   end
 
   def stripe_connect_setup
+    Rails.logger.warn "Params: #{params.inspect}"
+
     if params[:error].present?
+      Rails.logger.warn "params['error'].present? TRUE"
+
       flash[:error] = params[:error_description]
       redirect_to landing_url(subdomain: current_user.operator.subdomain)
     else
+      Rails.logger.warn "params['error'].present? FALSE"
+      Rails.logger.warn "params[:code] is #{params[:code]}"
+      Rails.logger.warn "current_user.operator is #{current_user.operator}"
+      Rails.logger.warn "stripe_webhooks_url is #{stripe_webhooks_url}"
+
       result = Operators::FinishStripeConnect.call(
         stripe_code: params[:code],
         operator: current_user.operator,
@@ -30,9 +39,12 @@ class LandingController < ApplicationController
         redirect_to landing_url(subdomain: current_user.operator.subdomain)
       else
         flash[:error] = "There was a problem storing your Stripe credentials. (#{result.message})"
+        Rails.logger.warn "current_user.operator.subdomain is #{current_user.operator.subdomain}"
         if current_user.operator.onboarded?
+          Rails.logger.warn "REDIRECTING TO modules_url"
           redirect_to modules_url(subdomain: current_user.operator.subdomain)
         else
+          Rails.logger.warn "REDIRECTING TO landing_url"
           redirect_to landing_url(subdomain: current_user.operator.subdomain)
         end
       end
