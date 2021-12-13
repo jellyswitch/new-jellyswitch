4ac21c3a2524a221904253ca9a631e7f1560b76d
Ben Damman <ben.damman@gmail.com>
2021-12-13 12:33:00 -0700

    Dumb fix for beginless raise error

--- lib/jellyswitch/usage_report.rb
+++ lib/jellyswitch/usage_report.rb
@@ -10,24 +10,32 @@ class Jellyswitch::UsageReport
     @reservations ||= user.reservations.this_month.group_by_day(:datetime_in).count.reject do |k,v|
       v < 1
     end
+  rescue
+    @reservations = []
   end
 
   def door_punches
     @door_punches ||= user.door_punches.this_month.group_by_day(:created_at).count.reject do |k,v|
       v < 1
     end
+  rescue
+    @door_punches = []
   end
 
   def checkins
     @checkins ||= user.checkins.this_month.group_by_day(:datetime_in).count.reject do |k,v|
       v < 1
     end
+  rescue
+    @checkins = []
   end
 
   def day_passes
     @day_passes ||= user.day_passes.this_month.group_by_day(:day).count.reject do |k,v|
       v < 1
     end
+  rescue
+    @day_passes = []
   end
 
   def days_used
