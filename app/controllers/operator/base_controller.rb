ef59d929434bf504ae9b735d288d0d62b42eaa34
Ben Damman <ben.damman@gmail.com>
2021-12-17 17:20:27 -0700

    Instrumentation for ActsAsScopable troubleshooting added

--- app/controllers/operator/base_controller.rb
+++ app/controllers/operator/base_controller.rb
@@ -62,7 +62,9 @@ class Operator::BaseController < ApplicationController
   def set_resource_scopes
     if ActsAsScopable.current_scope_resources.empty?
       ActsAsScopable.current_scope_resources = [current_tenant, current_location]
-      Rollbar.warning("ActsAsScopable.current_scope_resources failed to preload properly", current_tenant: current_tenant&.id, current_location: current_location&.id)
+      if current_tenant.nil? || current_location.nil?
+        Rollbar.warning("ActsAsScopable.current_scope_resources failed to preload properly", current_tenant: current_tenant&.id, current_location: current_location&.id)
+      end
     end
 
     if current_tenant.blank?
