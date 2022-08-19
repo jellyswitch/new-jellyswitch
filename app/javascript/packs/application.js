2b298696df5b18e5b0de465769f65dae089e1cc2
Timothy Carey <timcarey1989@gmail.com>
2022-08-19 17:47:30 -0400

    more descriptive fixture names, enabled credits on operator fixture, added comment in application.js for clarity on button fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,6 +36,9 @@ import 'bootstrap'
 
 import './pagy.js.erb'
 
+//https://stackoverflow.com/questions/46831525/how-to-keep-submit-buttons-disabled-on-remote-forms-until-the-next-page-has-load/46844912#46844912
+// This is to keep rails-ujs from re-enabling the checkout buttons on a turbolinks redirect
+
 ;(function () {
   var $doc = $(document)
 
