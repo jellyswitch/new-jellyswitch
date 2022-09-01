b9e23cec2a3cfab305adc4ff056ddee103bc8de7
Dave Paola <dpaola2@gmail.com>
2022-09-01 11:45:10 -0700

    rollback method

--- app/interactors/billing/credits/reset.rb
+++ app/interactors/billing/credits/reset.rb
@@ -4,6 +4,11 @@ class Billing::Credits::Reset
   delegate :creditable, to: :context
 
   def call
+    context.old_credit_balance = creditable.credit_balance
     creditable.update(credit_balance: 0)
+  end
+
+  def rollback
+    creditable.update(credit_balance: context.old_credit_balance)
   end
 end
