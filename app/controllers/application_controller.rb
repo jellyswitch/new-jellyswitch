9247bddf26d75e6fa7e81e2dafaba02825797a22
Dave Paola <dpaola2@gmail.com>
2022-09-01 13:48:33 -0700

    Fix broken authorization rescue flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def user_not_authorized
-    flash[:alert] = "Whoops! That's not allowed. If this isn't what you were expecting, please contact #{current_location.contact_name} by calling #{current_location.contact_phone}."
+    flash[:alert] = "Whoops! That's not allowed. If this isn't what you were expecting, please contact our staff."
     redirect_to referrer_or_root
   end
 
