00e08c600ee7fe6ff71c6f51fbc0d3a33a1ba932
Timothy Carey <timcarey1989@gmail.com>
2022-09-06 16:15:41 -0400

    added rooms controller test and rooms fixture

---
+++ test/controllers/rooms_controller_test.rb
@@ -0,0 +1,58 @@
+require "test_helper"
+
+class RoomsControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = users(:cowork_tahoe_admin)
+    log_in @user
+    @room = rooms(:cowork_tahoe_room)
+  end
+
+  test "should get rooms index" do
+    get rooms_path, env: default_env
+    assert_response :success
+  end
+
+  test "new room path should redirect unauthorized users" do
+    @user.update(admin: false)
+    get new_room_path, env: default_env
+    assert_redirected_to root_path
+  end
+
+  test "should show room" do
+    get room_path(@room), env: default_env
+    assert_response :success
+  end
+
+  test "should get new room path" do
+    get new_room_path, env: default_env
+    assert_response :success
+  end
+
+  test "should create a new room" do
+    assert_difference "Room.count", 1 do
+      post rooms_path, params: { room: { name: "tims room" } }, env: default_env
+    end
+    assert_not flash.empty?
+    assert_response :found
+    assert_redirected_to room_path(Room.last)
+  end
+
+  test "should not create room without a name" do
+    room = Room.new
+    assert_not room.save
+  end
+
+  test "should get room edit path" do
+    get edit_room_path(@room), env: default_env
+    assert_response :success
+  end
+
+  test "should update room" do
+    patch room_path(@room), params: { room: { name: "updated room name" } }, env: default_env
+    assert_redirected_to room_path(@room)
+    assert_not flash.empty?
+    @room.reload
+    assert_equal "updated room name", @room.name
+  end
+end
