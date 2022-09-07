3901270541727b1355dfe60cce9a68bf8a0c651f
Andrew Neidley <neidz44@gmail.com>
2022-09-07 13:45:47 -0500

    call new organizer from controller

--- app/controllers/operator/subscriptions_controller.rb
+++ app/controllers/operator/subscriptions_controller.rb
@@ -86,9 +86,8 @@ class Operator::SubscriptionsController < Operator::BaseController
     find_subscription
     authorize @subscription
 
-    result = CancelSubscription.call(
+    result = SetSubscriptionForCancellation.call(
       subscription: @subscription,
-      creditable: @subscription.subscribable,
       blob: { text: "#{@subscription.subscribable.name} canceled their membership", type: "post" },
       user: current_tenant.users.admins.first,
       operator: current_tenant,
