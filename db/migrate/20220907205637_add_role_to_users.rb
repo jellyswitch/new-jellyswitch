ed24ad367d6b27a8c1e2f2834e9a854f6fe33231
Andrew Neidley <neidz44@gmail.com>
2022-09-08 12:10:18 -0500

    change migration to be more explicit and use old columns in conditionals

--- db/migrate/20220907205637_add_role_to_users.rb
+++ db/migrate/20220907205637_add_role_to_users.rb
@@ -3,13 +3,13 @@ class AddRoleToUsers < ActiveRecord::Migration[6.1]
     add_column :users, :role, :string, null: false, default: "unassigned"
 
     User.all.map do |user|
-      if user.admin?
+      if user.admin == true
         user.update(role: 'admin')
       end
     end
 
     User.all.map do |user|
-      if user.superadmin?
+      if user.superadmin == true
         user.update(role: 'superadmin')
       end
     end
