c6cd8eaba22ff02ffabf24216555ece0f0fa9e8e
Dave Paola <dpaola2@gmail.com>
2022-09-08 12:01:44 -0700

    fix CORS issue and update chartkick

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,27 +1,13 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 import Rails from '@rails/ujs'
 import * as ActiveStorage from "@rails/activestorage"
 import Turbolinks from "turbolinks"
-// import "channels"
+
 import "trix"
 import "@rails/actiontext"
-import "chartkick/chart.js"
+import "chartkick"
+import "Chart.bundle"
 import ahoy from "ahoy.js"
 window.ahoy = ahoy
 import 'bootstrap'
