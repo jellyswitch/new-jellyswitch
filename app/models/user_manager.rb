5be3b5993d1148c48d226c0a497674404ceea34f
Dave Paola <dpaola2@gmail.com>
2022-09-23 11:43:47 -0700

    stripe  ruby mock + passing tests

--- app/models/user_manager.rb
+++ app/models/user_manager.rb
@@ -6,22 +6,36 @@ class UserManager
   end
 
   def ready
-    user.update(
-      name: name,
-      email: email,
-      bio: nil,
-      linkedin: nil,
-      twitter: nil,
-      website: nil,
-      phone: nil,
-      stripe_customer_id: nil,
-      archived: true,
-      card_added: false,
-      organization_id: nil,
-    )
+    ActiveRecord::Base.transaction do
+      user.update(
+        name: name,
+        email: email,
+        bio: nil,
+        linkedin: nil,
+        twitter: nil,
+        website: nil,
+        phone: nil,
+        stripe_customer_id: nil,
+        archived: true,
+        card_added: false,
+        organization_id: nil,
+      )
 
-    user.reservations.future.map do |reservation|
-      reservation.update(cancelled: true)
+      user.reservations.future.map do |reservation|
+        reservation.update(cancelled: true)
+      end
+
+      cancel_subscriptions!
+    end
+  end
+
+  def cancel_subscriptions!
+    failed_results = user.subscriptions.active.map do |subscription|
+      Billing::Subscription::CancelStripeSubscription.call(subscription: subscription)
+    end.select { |result| !result.success? }
+
+    if failed_results.count.positive?
+      raise "Error cancelling subscriptions: #{ failed_results.map(&:message).to_sentence }"
     end
   end
 
