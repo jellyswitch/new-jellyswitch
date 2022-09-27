6f72ca083ce749b631b2ffedec86c150cff70f43
Dave Paola <dpaola2@gmail.com>
2022-09-27 13:18:17 -0700

    fix one test

--- test/controllers/operator/users_controller_test.rb
+++ test/controllers/operator/users_controller_test.rb
@@ -8,12 +8,12 @@ class Operator::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should redirect to user show after user update" do
-    patch user_path(id: @user.id, params: { user_params: { user: @user, name: "New Name" } } ), env: default_env
+    patch user_path(@user, params: { user_params: { user: @user, name: "New Name" } } ), env: default_env
     assert_redirected_to user_path(@user)
   end
 
   test "should update the user with given params" do
-    patch user_path, params: { user: { id: @user.id, name: "New Name" } }, env: default_env
-    assert_equal "New Name", @user.name
+    patch user_path(@user, params: { user: { id: @user.id, name: "New Name" } }), env: default_env
+    assert_equal "New Name", @user.reload.name
   end
 end
