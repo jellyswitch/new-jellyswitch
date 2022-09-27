0eab6e5342c944e7e5b375d88ff6dedb0cbd438e
Timothy Carey <timcarey1989@gmail.com>
2022-09-27 16:43:27 -0400

    made test less brittle, removed comment

--- test/helpers/credit_helper_test.rb
+++ test/helpers/credit_helper_test.rb
@@ -6,11 +6,11 @@ class CreditHelperTest < ActionView::TestCase
     @room = rooms(:small_meeting_room)
     @reservation = reservations(:room_reservation)
     @duration = @reservation.minutes
-    @user = users(:cowork_tahoe_member) # User has 10 credits
+    @user = users(:cowork_tahoe_member)
 
     @new_balance = ending_balance(@user, reservation_cost(@room, @duration))
     @user.update(credit_balance: @new_balance)
 
-    assert_equal 7, @user.credit_balance
+    assert_equal @new_balance.to_i, @user.credit_balance
   end
 end
