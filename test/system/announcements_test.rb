f9a8e7d11c904205168cd619eef2050c6eacf030
Timothy Carey <timcarey1989@gmail.com>
2022-09-27 17:47:56 -0400

    announcements system test

---
+++ test/system/announcements_test.rb
@@ -0,0 +1,12 @@
+require 'application_system_test_case'
+
+class AnnouncementsTest < ApplicationSystemTestCase
+  test "posting announcement note" do
+    log_in(users(:cowork_tahoe_admin))
+    visit new_announcement_path
+    fill_in "Type a new announcement...", with: "Test announcement"
+    click_on "Post announcement"
+
+    assert_text "Test announcement"
+  end
+end
