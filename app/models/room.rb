7ddf35f110aaee9aaa0ed14f92b34f8351d4d1ef
Andrew Neidley <neidz44@gmail.com>
2022-09-28 15:14:39 -0500

    more refactor, show unavailable if room is not available_for_four_hour_duration? AND room does not allow shorter reservation durations

--- app/models/room.rb
+++ app/models/room.rb
@@ -84,31 +84,20 @@ class Room < ApplicationRecord
 
   # Instance Methods
 
-  def available_for_lowest_duration?(datetime_in:)
-    find_lowest_available_duration(datetime_in: datetime_in).present?
-  end
-
-  def lowest_duration_option
-    allow_shorter_reservation_duration ? 30 : 240
-  end
-
-  def find_lowest_available_duration(datetime_in:)
-    available_durations = []
+  def available_for_four_hour_duration?(datetime_in:)
     all_durations = [30, 60, 90, 120, 150, 180, 210, 240]
 
     all_durations.each do |duration|
       if available_at?(datetime_in + (duration.minutes - 1))
-        if duration == lowest_duration_option
-          available_durations << duration
-          break
+        if duration == 240
+          return true
         else
           next
         end
       else
-        break
+        return false
       end
     end
-    available_durations
   end
 
   def square_photo
