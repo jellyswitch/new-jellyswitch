2c701d97921875e199e7b3e2495a0df9eb8a7ab7
Andrew Neidley <neidz44@gmail.com>
2022-09-29 12:00:50 -0500

    duplicate cascading html.erb pattern, instead of confusing component logic

--- app/components/rooms/duration_buttons.rb
+++ app/components/rooms/duration_buttons.rb
@@ -9,32 +9,6 @@ class Rooms::DurationButtons < ApplicationComponent
     @user = user
   end
 
-  def allow_shorter_reservation_duration?
-    room.allow_shorter_reservation_duration?
-  end
-
-  def find_available_durations
-    available_durations = []
-    all_durations = [30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480]
-    duration_options = [30, 60, 90, 120, 180, 240, 480]
-
-    all_durations.each do |duration|
-      if room.available_at?(datetime_in + (duration.minutes - 1))
-        available_durations << duration
-      else
-        break
-      end
-    end
-
-    available_durations = available_durations.keep_if { |duration| duration_options.include?(duration) }
-
-    if !allow_shorter_reservation_duration?
-      available_durations = available_durations.keep_if { |duration| duration >= 240 }
-    end
-
-    available_durations
-  end
-
   private
 
   attr_reader :room, :datetime_in, :day, :hour, :user
