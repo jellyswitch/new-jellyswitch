220dbe6a702a90ad843b615c0a3cbc1582fa63f9
Dave Paola <dpaola2@gmail.com>
2022-09-29 08:25:34 -0700

    remove rest of typed annotations

--- app/policies/dashboard_policy.rb
+++ app/policies/dashboard_policy.rb
@@ -1,4 +1,4 @@
-# typed: ignore
+
 class DashboardPolicy < Struct.new(:user, :dashboard)
   include PolicyHelpers
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-# typed: strict
+
 Rails.application.routes.draw do
 
   namespace :mobile do
