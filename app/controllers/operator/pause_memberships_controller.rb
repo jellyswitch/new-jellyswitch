4488eb98a7bafb70e88ca9915ce8012c1445e0bb
Andrew Neidley <neidz44@gmail.com>
2022-10-04 10:57:56 -0500

    create pause_durations hash to secure and limit duration inputs and map values to integer durations

--- app/controllers/operator/pause_memberships_controller.rb
+++ app/controllers/operator/pause_memberships_controller.rb
@@ -4,8 +4,8 @@ class Operator::PauseMembershipsController < Operator::BaseController
   def create
     find_subscription
 
-    if params["resumes_at"].present?
-      resumes_at = params["resumes_at"].to_i
+    if pause_durations.has_key?(params["resumes_at"])
+      resumes_at = pause_durations[params["resumes_at"]].to_i
     else
       resumes_at = nil
     end
@@ -38,6 +38,16 @@ class Operator::PauseMembershipsController < Operator::BaseController
       flash[:error] = "Something went wrong unpausing your subscription '#{@subscription.plan.name}'"
       turbo_redirect subscription_path(@subscription)
     end
+  end
+
+  private
+
+  def pause_durations
+    {
+      "30" => (Time.current.to_i + 30.days),
+      "60" => (Time.current.to_i + 60.days),
+      "90" => (Time.current.to_i + 90.days)
+    }
   end
 
 end
