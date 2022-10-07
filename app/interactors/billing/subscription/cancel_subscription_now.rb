33ba030e03d61a01764c2576cb436d5cf7c2da83
Timothy Carey <timcarey1989@gmail.com>
2022-10-07 15:18:47 -0400

    fix organizer

--- app/interactors/billing/subscription/cancel_subscription_now.rb
+++ app/interactors/billing/subscription/cancel_subscription_now.rb
@@ -3,9 +3,9 @@ class Billing::Subscription::CancelSubscriptionNow
   include Interactor::Organizer
 
   organize(
-    Billing::Subscription::CancelStripeSubscription,
     FeedItems::Create,
     Billing::Credits::Reset,
-    CreateNotifications
-  )
+    CreateNotifications,
+    Billing::Subscription::CancelStripeSubscription,
+    )
 end
