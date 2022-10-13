6ec6fe0976dfd2ad9162a1fad9c3567db08163a3
Andrew Neidley <neidz44@gmail.com>
2022-10-13 11:19:57 -0500

    update scopes to reference string enum mapped values for better readability

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -45,9 +45,9 @@ class Subscription < ApplicationRecord
     joins(:plan).where(plans: {id: Plan.for_location(location).map(&:id) })
   end
   scope :for_week, -> (week_start, week_end) { where('created_at > ? and created_at <= ?', week_start, week_end) }
-  scope :not_paused, -> { where(paused: 0) }
-  scope :paused, -> { where(paused: 1) }
-  scope :pause_scheduled, -> { where(paused: 2) }
+  scope :not_paused, -> { where(paused: "false") }
+  scope :paused, -> { where(paused: "true") }
+  scope :pause_scheduled, -> { where(paused: "scheduled") }
 
   accepts_nested_attributes_for :plan
 
