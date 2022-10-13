7a0753364fd7e735331f0373e04224dbcec34b32
Andrew Neidley <neidz44@gmail.com>
2022-10-13 14:03:47 -0500

    fix syntax error

--- app/interactors/pause_memberships.rb
+++ app/interactors/pause_memberships.rb
@@ -2,7 +2,7 @@ class PauseMemberships
   include Interactor
 
   def call
-    Subscription.pause_scheduled.map |subscription| do
+    Subscription.pause_scheduled.map do |subscription|
       if subscription.current_period_end.today? || subscription.current_period_end.tomorrow?
         PauseMembership.call(subscription: subscription)
       end
