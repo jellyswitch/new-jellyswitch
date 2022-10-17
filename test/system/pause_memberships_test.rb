37abff8d608a861a9de1ef3b02533db26f6cd629
Andrew Neidley <neidz44@gmail.com>
2022-10-17 16:38:00 -0500

    remove pausing/unpausing from views for now + remove pausing system tests

--- test/system/pause_memberships_test.rb
+++ test/system/pause_memberships_test.rb
@@ -1,62 +1,62 @@
 require "application_system_test_case"
 
 class PauseMembershipsTest < ApplicationSystemTestCase
-  setup do
-    StripeMock.start
-  end
+  # setup do
+  #   StripeMock.start
+  # end
   
-  test "user can see option to pause membership from change my account screen" do
-    @user = users(:cowork_tahoe_member)
-    setup_stripe
-    log_in(@user)
+  # test "user can see option to pause membership from change my account screen" do
+  #   @user = users(:cowork_tahoe_member)
+  #   setup_stripe
+  #   log_in(@user)
 
-    click_on 'My Account'
-    assert_text "Change My Account"
+  #   click_on 'My Account'
+  #   assert_text "Change My Account"
     
-    click_on 'Change My Account'
-    assert_text "Pause Membership"
-  end
+  #   click_on 'Change My Account'
+  #   assert_text "Pause Membership"
+  # end
   
-  test "if paused, user can see option to unpause membership from change my account screen" do
-    @user = users(:cowork_tahoe_member)
-    setup_stripe
-    log_in(@user)
+  # test "if paused, user can see option to unpause membership from change my account screen" do
+  #   @user = users(:cowork_tahoe_member)
+  #   setup_stripe
+  #   log_in(@user)
 
-    click_on 'My Account'
-    assert_text "Change My Account"
+  #   click_on 'My Account'
+  #   assert_text "Change My Account"
     
-    click_on 'Change My Account'
-    assert_text "Pause Membership"
+  #   click_on 'Change My Account'
+  #   assert_text "Pause Membership"
     
-    click_on 'Pause Membership'
-    assert_text "You have paused your subscription"
-    assert_text 'My Account'
+  #   click_on 'Pause Membership'
+  #   assert_text "You have paused your subscription"
+  #   assert_text 'My Account'
     
-    click_on 'My Account'
-    assert_text "Change My Account"
+  #   click_on 'My Account'
+  #   assert_text "Change My Account"
     
-    click_on 'Change My Account'
-    assert_text "Unpause Membership"
-  end
+  #   click_on 'Change My Account'
+  #   assert_text "Unpause Membership"
+  # end
 
-  test "user can see option to pause membership from cancel membership modal" do
-    @user = users(:cowork_tahoe_member)
-    setup_stripe
-    log_in(@user)
+  # test "user can see option to pause membership from cancel membership modal" do
+  #   @user = users(:cowork_tahoe_member)
+  #   setup_stripe
+  #   log_in(@user)
 
-    click_on 'My Account'
-    assert_text "View my membership"
+  #   click_on 'My Account'
+  #   assert_text "View my membership"
 
-    click_on "View my membership"
-    assert_text "Cancel membership"
+  #   click_on "View my membership"
+  #   assert_text "Cancel membership"
 
-    click_on "Cancel membership"
-    assert_text "Pause Membership"
+  #   click_on "Cancel membership"
+  #   assert_text "Pause Membership"
 
-    click_on "Pause Membership"
-    assert_text "You have paused your subscription"
-    assert_text "My Account"
-  end
+  #   click_on "Pause Membership"
+  #   assert_text "You have paused your subscription"
+  #   assert_text "My Account"
+  # end
 
   # test "if paused, user can see option to unpause membership from cancel membership modal" do
   #   @user = users(:cowork_tahoe_member)
