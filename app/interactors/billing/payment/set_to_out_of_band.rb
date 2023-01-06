810e3d53bc5329d242595ebfb90bffa203751b68
Andrew Neidley <neidz44@gmail.com>
2023-01-06 10:23:50 -0500

    set_to_out_of_band sets card_added to false

--- app/interactors/billing/payment/set_to_out_of_band.rb
+++ app/interactors/billing/payment/set_to_out_of_band.rb
@@ -5,7 +5,7 @@ class Billing::Payment::SetToOutOfBand
   delegate :user, to: :context
 
   def call
-    if !user.update(out_of_band: true, bill_to_organization: false)
+    if !user.update(card_added: false, out_of_band: true, bill_to_organization: false)
       context.fail!(message: "An error occurred.")
     end
   end
