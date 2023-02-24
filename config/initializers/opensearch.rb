39eaad059c7a54a0d4f3059541884091a8c78be7
Dave Paola <dpaola2@gmail.com>
2023-02-24 14:08:46 -0800

    include bonsai green url

--- config/initializers/opensearch.rb
+++ config/initializers/opensearch.rb
@@ -1,4 +1,4 @@
 # difference between production and staging environments
 
 # Staging uses Bonsai add-on
-ENV["OPENSEARCH_URL"] = ENV["BONSAI_URL"] if ENV["BONSAI_URL"]
+ENV["OPENSEARCH_URL"] = ENV["BONSAI_URL"] || ENV["BONSAI_GREEN_URL"]
