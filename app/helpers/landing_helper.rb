e61f70ab485e0f77dda432bb6e1e66cb9a67db91
JP <85654561+jp524@users.noreply.github.com>
2024-03-22 14:06:14 -0400

    Refactor logic in LandingHelper#landing_redirect
    Co-authored-by: Josh Smith <jmsmith1018@gmail.com>

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -1,30 +1,30 @@
 
 module LandingHelper
   def landing_redirect
-    if logged_in? && current_location.present?
-      if admin? || community_manager? || general_manager?
-        if current_tenant.onboarded? || current_tenant.skip_onboarding?
-          redirect_to feed_items_path
+    return unless logged_in? && current_location.present?
+
+    if admin? || community_manager? || general_manager?
+      if current_tenant.onboarded? || current_tenant.skip_onboarding?
+        redirect_to feed_items_path
+      else
+        redirect_to new_operator_onboarding_path
+      end
+    else
+      if current_user.allowed_in?(current_location)
+        if approved?
+          if current_user_requires_check_in?
+            redirect_to required_checkins_path
+          else
+            redirect_to home_path
+          end
         else
-          redirect_to new_operator_onboarding_path
+          redirect_to wait_path
         end
       else
-        if current_user.allowed_in?(current_location)
-          if approved?
-            if current_tenant.checkin_required? && !current_user.checked_in?(current_location)
-              redirect_to required_checkins_path
-            else
-              redirect_to home_path
-            end
-          else
-            redirect_to wait_path
-          end
+        if pending?
+          redirect_to activate_path
         else
-          if pending?
-            redirect_to activate_path
-          else
-            redirect_to choose_path
-          end
+          redirect_to choose_path
         end
       end
     end
