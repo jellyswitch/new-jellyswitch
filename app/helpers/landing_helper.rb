e91a04ec70623fff557f074ed835fc40a694b853
JP <85654561+jp524@users.noreply.github.com>
2024-03-22 13:52:11 -0400

    Refactor logic in LandingHelper#home_redirect
    Co-authored-by: Josh Smith <jmsmith1018@gmail.com>

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -31,15 +31,15 @@ module LandingHelper
   end
 
   def home_redirect
-    if current_location.present? && (current_user.allowed_in?(current_location) || (!policy(:payment).enabled? && current_tenant.subdomain != "southlakecoworking"))
-      if !approved? && !admin?
-        redirect_to wait_path
+    return unless current_location.present?
+    
+    if current_user&.allowed_in?(current_location) || payment_policy_enabled_for_southlakecoworking?
+      redirect_to wait_path and return unless approved? && admin?
+
+      if !admin? && !always_has_access? && current_user_requires_check_in?
+        redirect_to required_checkins_path
       else
-        if !admin? && !always_has_access? && current_tenant.checkin_required? && !current_user.checked_in?(current_location)
-          redirect_to required_checkins_path
-        else
-          render :home
-        end
+        render :home
       end
     else
       if logged_in?
@@ -63,5 +63,13 @@ module LandingHelper
 
   def always_has_access?
     current_user.has_building_access_lease? || current_user.always_allow_building_access?
+  end
+
+  def payment_policy_enabled_for_southlakecoworking?
+    !policy(:payment).enabled? && current_tenant.subdomain != "southlakecoworking"
+  end
+
+  def current_user_requires_check_in?
+    current_tenant.checkin_required? && !current_user.checked_in?(current_location)
   end
 end
