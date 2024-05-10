0db94cd81315d33e2e1dfde7ba3aa67f6d79aa43
Dao Tran <dao.tran@eastagile.com>
2024-05-10 11:54:59 +0700

    [f] 520 - Admin ability to provision FREE DAY PASS for member account without member adding payment information
    https://www.pivotaltracker.com/story/show/187513520

--- app/interactors/billing/day_passes/save_day_pass.rb
+++ app/interactors/billing/day_passes/save_day_pass.rb
@@ -1,30 +1,32 @@
-
 class Billing::DayPasses::SaveDayPass
   include Interactor
 
   delegate :day_pass, :token, :operator, :out_of_band, :params, :user_id, to: :context
 
   def call
-    user = User.find(user_id)
+    user = User.find_by(id: user_id)
     if user.nil?
       context.fail!(message: "No such user with ID #{user_id}")
     end
     context.user = user
 
-    day_pass_type = DayPassType.find(params[:day_pass_type].to_i)
+    day_pass_type = DayPassType.find_by(id: params[:day_pass_type].to_i)
     if day_pass_type.nil?
       context.fail!(message: "Invalid day pass type.")
     end
 
-    day_pass = DayPass.new(params.merge({day_pass_type: day_pass_type}))
+    day_pass = DayPass.new(params.merge({ day_pass_type: day_pass_type }))
     day_pass.user = user
     day_pass.billable = BillableFactory.for(day_pass).billable
 
-    context.fail!(message: "Cannot create day pass for user without billing info.") unless day_pass.billable.has_stripe_customer?
+    unless day_pass_type.free? || day_pass.billable.has_stripe_customer?
+      context.fail!(message: "Cannot create paid day pass for user without billing info.")
+    end
 
     if !day_pass.save
       context.fail!(message: "Unable to create day pass.")
     end
+
     context.day_pass = day_pass
   end
 
