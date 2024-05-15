1b7e3d2839974a6055a422feb333e58b74b15427
Dao Tran <dao.tran@eastagile.com>
2024-05-15 14:50:25 +0700

    [b] 842 - Fix changing to paying by credit card does not update in Stripe
    https://www.pivotaltracker.com/story/show/187578842

--- app/interactors/billing/payment/update_user_payment.rb
+++ app/interactors/billing/payment/update_user_payment.rb
@@ -9,6 +9,12 @@ class Billing::Payment::UpdateUserPayment
       user.update(out_of_band: true)
     else
       if token
+        user.subscriptions_billable.active.each do |subscription|
+          stripe_subscription = subscription.stripe_subscription
+          stripe_subscription.billing = "charge_automatically"
+          stripe_subscription.save
+        end
+
         if user.operator.create_or_update_customer_payment(user, token)
           user.update(card_added: true, out_of_band: false)
         else
