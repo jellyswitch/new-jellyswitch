ab813d8852639f24c3c554d57298a02df6a1919c
Dao Tran <dao.tran@eastagile.com>
2024-06-14 17:13:49 +0700

    [c] 607 - Writing automation tests for the 'Reserve Now' flow
    https://www.pivotaltracker.com/story/show/187781607

--- test/helpers/reservation_helper_test.rb
+++ test/helpers/reservation_helper_test.rb
@@ -4,9 +4,6 @@ class ReservationHelperTest < ActionView::TestCase
   include ReservationHelper
 
   def setup
-    @start_hour_time = 6
-    @end_hour_time = 20
-
     @now = Time.zone.parse("2024-06-15 10:00:00")
     Timecop.freeze(@now)
   end
@@ -15,8 +12,8 @@ class ReservationHelperTest < ActionView::TestCase
     Timecop.return
   end
 
-  test "should returns array of available timeslot within 15-minute intervals" do
-    time_slots = calculate_available_time_slots(Time.zone.today)
+  test "should returns array of available timeslot within 15-minute intervals for day" do
+    time_slots = calculate_available_time_slots(Time.zone.today, "day")
 
     expected_start_time = Time.zone.parse("2024-06-15 10:15:00")
     assert_equal time_slots.first, expected_start_time
@@ -25,7 +22,15 @@ class ReservationHelperTest < ActionView::TestCase
       assert_equal current_slot + 15.minutes, next_slot, "Time slots should be in 15-minute intervals"
     end
 
-    expected_end_time = Time.zone.parse("2024-06-15 #{@end_hour_time}:00:00")
+    expected_end_time = Time.zone.parse("2024-06-15 11:45:00")
     assert_equal time_slots.last, expected_end_time
+  end
+
+  test "should return the nearest available time slot" do
+    date = Time.zone.today
+    nearest_time_slot = calculate_nearest_time_slot(date)
+
+    expected_time_slot = Time.zone.parse("2024-06-15 10:15:00")
+    assert_equal nearest_time_slot, expected_time_slot
   end
 end
---
+++ test/system/reservation_by_calendar_test.rb
@@ -0,0 +1,51 @@
+require 'application_system_test_case'
+
+class ReservationByCalendarTest < ApplicationSystemTestCase
+  setup do
+    @room = rooms(:small_meeting_room)
+    @day = Time.zone.tomorrow
+
+    @time = "10:00"
+
+    @duration = "1.5 hours"
+    @duration_minutes = 90
+
+    operator = operators(:cowork_tahoe)
+    operator.update(credits_enabled: false)
+  end
+
+  test "users go through the reserve by calendar flow and create reservation successfully" do
+    StripeMock.start
+
+    @user = users(:cowork_tahoe_member)
+    log_in @user
+
+    setup_stripe
+
+    click_on 'Reserve Later'
+
+    assert_text 'Reservation Date'
+
+    find(".fc-day-top[data-date='#{@day.strftime("%Y-%m-%d")}']").click
+
+    assert_text 'Reservation Details'
+    assert_text @day.strftime("%B %d, %Y")
+
+    find(".form-check-label", text: "Night (PM)").click
+    find(".time-slot", text: @time).click
+
+    assert_text 'Meeting Duration'
+    find(".duration-slot", text: @duration).click
+
+    assert_text 'Available Room'
+
+    select @room.name, from: 'room_id'
+
+    click_on 'Confirm'
+
+    assert_text('Reservation Confirmation')
+
+    assert_text("#{@user.name} in #{@room.name}")
+    assert_text("#{@day.strftime("%m/%d/%Y")} at #{@time}pm for #{@duration_minutes} minutes")
+  end
+end
