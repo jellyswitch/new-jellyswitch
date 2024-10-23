9169816be5824fc5dcc78b0357712513ae2a513f
ea-open-source <open-source@eastagile.com>
2024-10-23 08:45:26 +0700

    Attempt to fix redis SSL url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 
 redis_url = ENV['REDIS_URL'] || "redis://localhost:6379"
 $redis = Redis.new(url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+ActiveJob::TrafficControl.client = Redis.new(url: redis_url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
