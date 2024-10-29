33981ea627eb873ed48d8158d9b7636e032698ac
Thien Lam <thien.lam@eastagile.com>
2024-10-29 15:08:32 +0700

    379 - As a user, I want to see announcements specific to my location only
    https://www.pivotaltracker.com/story/show/188479379

--- test/system/announcements_test.rb
+++ test/system/announcements_test.rb
@@ -1,12 +1,29 @@
 require 'application_system_test_case'
 
 class AnnouncementsTest < ApplicationSystemTestCase
-  # test "posting announcement note" do
-  #   log_in(users(:cowork_tahoe_admin))
-  #   visit new_announcement_path
-  #   fill_in "text", with: "Test announcement"
-  #   click_on "Post announcement"
+  test "posting announcement note" do
+    log_in(users(:cowork_tahoe_admin))
+    operator = operators(:cowork_tahoe)
+    other_location = create(:location, operator: operator)
+    visit new_announcement_path
+    fill_in "text", with: "Test announcement"
+    click_on "Post announcement"
 
-  #   assert_text "Test announcement"
-  # end
+    assert_text "Test announcement"
+
+    # user sees announcement at the location
+    visit home_path
+    assert_text "posted an announcement"
+    assert_text "Test announcement"
+
+    # change location
+    click_on "Change Location"
+    page.find_button(other_location.name).click
+    wait_for_turbo
+
+    # user sees no announcement at the other location
+    visit home_path
+    assert_no_text "posted an announcement"
+    assert_no_text "Test announcement"
+  end
 end
