0f8975d276c4464446cd0e98616c9287a9ca9b06
Thien Lam <thien.lam@eastagile.com>
2024-10-30 09:28:18 +0700

    [r] Ensure feed items are filtered by location and fix stuff

--- test/system/announcements_test.rb
+++ test/system/announcements_test.rb
@@ -16,14 +16,28 @@ class AnnouncementsTest < ApplicationSystemTestCase
     assert_text "posted an announcement"
     assert_text "Test announcement"
 
+    # admin sees the corresponding feed item
+    visit feed_items_path
+    assert_text "posted an announcement"
+    assert_text "Test announcement"
+
     # change location
-    click_on "Change Location"
+    if page.has_link?("Change Location")
+      click_on "Change Location"
+    else
+      find(".navbar-toggler").click
+      click_on "Change Location"
+    end
     page.find_button(other_location.name).click
     wait_for_turbo
 
     # user sees no announcement at the other location
     visit home_path
     assert_no_text "posted an announcement"
+    assert_no_text "Test announcement"
+
+    # admin sees no feed item at the other location
+    visit feed_items_path
     assert_no_text "Test announcement"
   end
 end
