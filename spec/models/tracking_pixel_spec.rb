9cf034c6153189cd1964a4ff2cffab0aa3a700e3
Thien Lam <thien.lam@eastagile.com>
2024-11-22 09:34:31 +0700

    Refactor and add more tests

--- spec/models/tracking_pixel_spec.rb
+++ spec/models/tracking_pixel_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe TrackingPixel, type: :model do
   describe 'associations' do
-    it { is_expected.to belong_to(:operator) }
     it { is_expected.to belong_to(:location).optional }
   end
 
