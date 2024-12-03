82570959ba6549b6cbffdb8d3fd5c613725229ac
Thien Lam <thien.lam@eastagile.com>
2024-12-03 09:55:59 +0700

    [b] 648 - Bug - cannot mark invoice as paid
    https://www.pivotaltracker.com/story/show/188641648

--- app/interactors/billing/invoices/mark_invoice_as_paid.rb
+++ app/interactors/billing/invoices/mark_invoice_as_paid.rb
@@ -5,18 +5,22 @@ class Billing::Invoices::MarkInvoiceAsPaid
   delegate :invoice, :operator, to: :context
 
   def call
-    if operator.mark_invoice_paid(invoice, paid_out_of_band: true)
-      invoice.update(status: 'paid')
+    if invoice.location
+      if invoice.location.mark_invoice_paid(invoice, paid_out_of_band: true)
+        invoice.update(status: 'paid')
 
-      result = Billing::Invoices::AddCreditsToSubscribable.call(
-        invoice: invoice
-      )
+        result = Billing::Invoices::AddCreditsToSubscribable.call(
+          invoice: invoice
+        )
 
-      if !result.success?
-        context.fail!(message: result.message)
+        if !result.success?
+          context.fail!(message: result.message)
+        end
+      else
+        context.fail!(message: 'Failed to mark invoice as paid.')
       end
     else
-      context.fail!(message: 'Failed to mark invoice as paid.')
+      context.fail!(message: 'Invoice location is missing')
     end
   end
 end
