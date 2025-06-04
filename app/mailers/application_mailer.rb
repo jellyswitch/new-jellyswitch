dd98bdaf2b3e3d672ac3118234379ca1c21de80e
Hung Duong <hung.duong@eastagile.com>
2025-06-04 16:06:52 +0700

    [b] Test disable clicktrack

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,15 @@
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'Jellyswitch <noreply@jellyswitch.com>'
+  default from: 'Jellyswitch <noreply@jellyswitch.com>',
+          'X-SMTPAPI' => {
+            "filters" => {
+              "clicktrack" => {
+                "settings" => {
+                  "enable" => 0
+                }
+              }
+            }
+          }.to_json
   layout 'mailer'  
 
   
