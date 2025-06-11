94c45ba4bbc805a733cb6c2f863c142d26a3b21c
Hung Duong <hung.duong@eastagile.com>
2025-06-11 12:03:46 +0700

    [f] Update email template so it does not go to spam

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,21 @@ class UserMailer < ApplicationMailer
   def password_reset(user, operator)
     @user = user
     @operator = operator
-
-    mail to: user.email, subject: "#{@operator.name} password reset", from: "noreply@jellyswitch.com", reply_to: @operator.contact_email
+    mail to: user.email, subject: "#{@operator.name} password reset", from: 'Jellyswitch <noreply@jellyswitch.com>', reply_to: @operator.contact_email,
+    'X-SMTPAPI' => {
+      "filters" => {
+        "clicktrack" => {
+          "settings" => {
+            "enable" => 0
+          }
+        },
+        "opentrack" => {
+          "settings" => {
+            "enable" => 0
+          }
+        }
+      }
+    }.to_json
     recipients = User.superadmins.all.map {|u| u.email }
   end
 
@@ -24,7 +37,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @event_name = event_name
     @operator = operator
-    
+
     @host = ENV['ASSET_HOST']
     mail to: @user.email, subject: "Cancelled: #{@event_name}", from: "noreply@jellyswitch.com", reply_to: @operator.contact_email
   end
