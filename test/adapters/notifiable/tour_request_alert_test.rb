8d5afd3805b2c6894e1dcd96b0f31907faebd4ac
DROARR <David@TahoeMountainLab.com>
2026-05-18 00:00:22 -0700

    Notifiable::TourRequestAlert: push fan-out to admins + location managers
    Adds adapter + tests for tour-request push (and later email) notifications.
    Recipients = operator admins union GMs/community managers at the requested
    location, de-duplicated. send_email guarded by defined?(TourRequestMailer)
    until Task 11 lands.

---
+++ test/adapters/notifiable/tour_request_alert_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class Notifiable::TourRequestAlertTest < ActiveSupport::TestCase
+  setup do
+    setup_initial_user_fixtures
+    @operator = operators(:cowork_tahoe)
+    @location = @operator.locations.first
+    @admin    = users(:cowork_tahoe_admin)
+    @gm       = User.create!(
+      email: "gm+tour@example.com", name: "GM Test", operator: @operator,
+      role: User::GENERAL_MANAGER, current_location_id: @location.id,
+      original_location_id: @location.id, admin_created: true, password: "tempPass1!", phone: "555-0000",
+    )
+    @requester = User.create!(
+      email: "req+tour@example.com", name: "Req Test", operator: @operator,
+      original_location_id: @location.id, admin_created: true, password: "tempPass1!", phone: "555-0001",
+    )
+    @activity = Activity.create!(
+      user: @requester, operator: @operator, kind: "tour_request",
+      occurred_at: Time.current, subject: @location, payload: { "message" => "Curious about hot desks" },
+    )
+    @notifiable = Notifiable::TourRequestAlert.new(@activity)
+  end
+
+  test "recipients includes operator admins" do
+    assert_includes @notifiable.send(:recipients), @admin
+  end
+
+  test "recipients includes general managers at the requested location" do
+    assert_includes @notifiable.send(:recipients), @gm
+  end
+
+  test "recipients excludes managers at other locations" do
+    other_location = @operator.locations.create!(name: "Other", visible: true, time_zone: "Pacific Time (US & Canada)")
+    other_gm = User.create!(
+      email: "other-gm@example.com", name: "Other GM", operator: @operator,
+      role: User::GENERAL_MANAGER, current_location_id: other_location.id,
+      original_location_id: other_location.id, admin_created: true, password: "tempPass1!", phone: "555-9999",
+    )
+    refute_includes @notifiable.send(:recipients), other_gm
+  end
+
+  test "recipients de-duplicates if a user is both admin and GM" do
+    @admin.update!(role: User::GENERAL_MANAGER, current_location_id: @location.id)
+    recipients = @notifiable.send(:recipients)
+    assert_equal recipients.uniq, recipients
+  end
+
+  test "should_send_notification? true only for tour_request kind" do
+    assert @notifiable.send(:should_send_notification?)
+    other = Activity.create!(user: @requester, operator: @operator, kind: "signup", occurred_at: Time.current, payload: {})
+    refute Notifiable::TourRequestAlert.new(other).send(:should_send_notification?)
+  end
+
+  test "message includes requester name and message preview" do
+    msg = @notifiable.send(:message)
+    assert_includes msg, "Req Test"
+    assert_includes msg.downcase, "tour request"
+  end
+end
